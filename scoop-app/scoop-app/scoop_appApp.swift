30f50a76f15e1e8307f2d6615e57a2cceffae55f
kris-randen <kris.randen@gmail.com>
2022-07-22 10:32:36 +0530

    Created Xcode project for scoop-app.

---
+++ scoop-app/scoop-app/scoop_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoop_appApp.swift
+//  scoop-app
+//
+//  Created by Krishnaswami Rajendren on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct scoop_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
